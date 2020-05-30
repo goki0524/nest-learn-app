66dfd1fd58b61e3f82a271f8411c60aa14fb781f
goki sakaguchi <goki0524@gmail.com>
2020-05-30 17:03:46 +0900

    Basic CURD API

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      // name: `${process.env.npm_package_name}-v${process.env.npm_package_version}`,
+      // script: 'dist/main.js',
+      // listen_timeout: 10000, // 10 seconds
+      // error_file: 'logs/err.log',
+      // out_file: 'logs/out.log',
+      // log_file: 'logs/combined.log',
+      // log_date_format: 'YYYY-MM-DD HH:mm:ss:SSS',
+      // min_uptime: 10000, // 10 seconds
+      // max_restarts: 3,
+      // max_memory_restart: '2G', // will restart if memory uses 2GB
+      // // use node at nvm
+      // interpreter: `node@${process.env.npm_package_engines_node}`,
+    },
+  ],
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import helmet from 'helmet'
+
+const PORT = 3000
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  app.use(helmet())
+
+  if (process.env.ENV !== 'production') {
+    const options = new DocumentBuilder()
+      .setTitle('Nest.js learn app')
+      .setDescription('Nest.js learn app description')
+      .setVersion('1.0')
+      .build()
+
+    const document = SwaggerModule.createDocument(app, options)
+    SwaggerModule.setup('swagger', app, document)
+  }
+
+  await app.listen(PORT)
+  console.log(`Server listening on: http://localhost:${PORT}/`)
 }
-bootstrap();
+bootstrap()
