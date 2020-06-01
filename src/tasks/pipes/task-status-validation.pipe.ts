a7f0ffcd2e03c99135ac00336492d7038684169e
goki sakaguchi <goki0524@gmail.com>
2020-06-01 21:41:04 +0900

    Add TypeOrm
    CRUD using typeorm

--- src/tasks/pipes/task-status-validation.pipe.ts
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -1,23 +1,19 @@
-import { PipeTransform, BadRequestException } from "@nestjs/common";
-import { TaskStatus } from "../task.model";
+import { PipeTransform, BadRequestException } from '@nestjs/common'
+import { TaskStatus } from '../task-status.enum'
 
 export class TaskStatusValidationPipe implements PipeTransform {
-  readonly allowrdStatuses = [
-    TaskStatus.OPEN,
-    TaskStatus.IN_PROGRESS,
-    TaskStatus.DONE,
-  ]
+  readonly allowrdStatuses = [TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE]
 
   transform(value: any) {
     value = value.toUpperCase()
 
-    if(!this.isStatusValid(value)) {
+    if (!this.isStatusValid(value)) {
       throw new BadRequestException(`"${value}" is an invalid status`)
     }
 
     return value
   }
-  
+
   private isStatusValid(status: any) {
     const idx = this.allowrdStatuses.indexOf(status)
     return idx !== -1
