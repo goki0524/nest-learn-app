40c2db3c31bf8f9c283c97734403350f6c55eddc
goki sakaguchi <goki0524@gmail.com>
2020-06-02 22:08:11 +0900

    Add Logger

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,32 @@
 import { NestFactory } from '@nestjs/core'
+import { Logger } from '@nestjs/common'
 import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import helmet from 'helmet'
 
-const PORT = 3000
-
 async function bootstrap() {
+  // Set up Logger
+  const logger = new Logger('bootstrap')
+
+  // Set up Nestjs
   const app = await NestFactory.create(AppModule)
+
+  // Set up helmet
   app.use(helmet())
 
+  // Set up Swagger
   if (process.env.ENV !== 'production') {
     const options = new DocumentBuilder()
       .setTitle('Nest.js learn app')
       .setDescription('Nest.js learn app description')
       .setVersion('1.0')
       .build()
-
     const document = SwaggerModule.createDocument(app, options)
     SwaggerModule.setup('swagger', app, document)
   }
 
-  await app.listen(PORT)
-  console.log(`Server listening on: http://localhost:${PORT}/`)
+  const port = 3000
+  await app.listen(port)
+  logger.log(`Application listenig on http://localhost:${port}/`)
 }
 bootstrap()
